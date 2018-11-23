b49a9e4c9a4105cda89754d85561529e25cb12d5
karshan96 <karshanguna@gmail.com>
2018-11-23 11:38:25 +0530

    User logIn activity design finished

--- app/src/main/java/com/abc/life_saver/MainActivity.java
+++ app/src/main/java/com/abc/life_saver/MainActivity.java
@@ -1,13 +1,22 @@
 package com.abc.life_saver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.Register);
+    }
+
+    public void registerUser(View v){
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
     }
 }
